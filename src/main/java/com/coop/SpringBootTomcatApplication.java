99ee705d8e3ca263867d7634da97328250b79246
Mateo Plante <Mateo_plante_xd@hotmail.com>
2019-03-18 19:41:30 -0300

    Last Fixx

--- src/main/java/com/coop/SpringBootTomcatApplication.java
+++
@@ -1,9 +0,0 @@
-package com.coop;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-@SpringBootApplication
-public class SpringBootTomcatApplication extends SpringBootServletInitializer {
-
-}
